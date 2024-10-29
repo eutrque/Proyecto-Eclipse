6834fab5a937008191c2dfcff450e7e5667422b1
Estela Utrera <eutrque@g.educaand.es>
2024-10-29 10:20:47 +0100

    Modificacion2 hola.java

--- src/com/google/eclipse/vistas/hola.java
+++ src/com/google/eclipse/vistas/hola.java
@@ -4,7 +4,8 @@ public class hola {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!!!");
-
+		System.out.println("Hola mundo!!!");
+		System.out.println("Hola mundo!!!");
 	}
 
 }
