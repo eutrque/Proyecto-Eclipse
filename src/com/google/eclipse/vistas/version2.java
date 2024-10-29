3e2aaab03b369ff8fbdfb89fac57499452b7e6a7
Estela Utrera <eutrque@g.educaand.es>
2024-10-29 10:53:28 +0100

    version2 nueva clase

---
+++ src/com/google/eclipse/vistas/version2.java
@@ -0,0 +1,13 @@
+package com.google.eclipse.vistas;
+
+public class version2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo!!!!");
+		System.out.println("Hola mundo!!!!");
+		System.out.println("Hola mundo!!!!");
+		System.out.println("Hola mundo!!!!");
+	}
+
+}
